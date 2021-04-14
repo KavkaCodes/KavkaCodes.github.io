d4fd5bbe1d9c4322851ad375045bc7e2666d6af4
SumedhUdar <sumedh.udar2000@gmail.com>
2021-04-14 17:23:04 +0530

    Big Screen

--- js/event.js
+++
@@ -1,7 +0,0 @@
-var solutions = document.getElementById("solutions");
-var flockT = document.getElementById("flockT");
-// var solutions = document.getElementById("solutions");
-// var solutions = document.getElementById("solutions");
-solutions.addEventListener("click", ()=>{
-    window.open("https://solutions-is.com/");
-})
